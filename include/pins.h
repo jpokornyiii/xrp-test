7be4336801753f89af117876e5158b08563ae497
Joe Pokorny <jpokornyiii@gmail.com>
2025-03-17 22:56:47 -0400

    fixing motors for prod

--- include/pins.h
+++ include/pins.h
@@ -7,9 +7,43 @@
 // Ultra Sonic max pulse
 #define ULTRASONIC_MAX_PULSE_WIDTH 23200
 
-// Servo pulses
-#define XRP_SERVO_MIN_PULSE_US 500
-#define XRP_SERVO_MAX_PULSE_US 2500
+// LED_BUILTIN is defined in the board to 
+// the correct pin 
+#define XRP_BUILTIN_LED LED_BUILTIN
+
+// Wpilib Motor mappings
+#define WPILIB_CH_PWM_MOTOR_L 0
+#define WPILIB_CH_PWM_MOTOR_R 1
+#define WPILIB_CH_PWM_MOTOR_3 2
+#define WPILIB_CH_PWM_MOTOR_4 3
+#define WPILIB_CH_PWM_SERVO_1 4
+#define WPILIB_CH_PWM_SERVO_2 5
+#define WPILIB_CH_PWM_SERVO_3 6
+#define WPILIB_CH_PWM_SERVO_4 7
+
+// WPILib Channel mappings
+#define WPILIB_ENCODER_L_CH_A 4
+#define WPILIB_ENCODER_L_CH_B 5
+#define WPILIB_ENCODER_R_CH_A 6
+#define WPILIB_ENCODER_R_CH_B 7
+#define WPILIB_ENCODER_3_CH_A 8
+#define WPILIB_ENCODER_3_CH_B 9
+#define WPILIB_ENCODER_4_CH_A 10
+#define WPILIB_ENCODER_4_CH_B 11
+
+// Encoder State Machine indices
+#define ENC_SM_IDX_MOTOR_L 0
+#define ENC_SM_IDX_MOTOR_R 1
+#define ENC_SM_IDX_MOTOR_3 2
+#define ENC_SM_IDX_MOTOR_4 3
+
+#define NUM_OF_ENCODERS 4
+#define NUM_OF_SERVOS 4
+
+#define XRP_DATA_ENCODER 0x01
+#define XRP_DATA_DIO 0x02
+#define XRP_DATA_AIO 0x04
+#define XRP_DATA_GENERAL 0x08
 
 #ifdef PICO_RP2350
 
@@ -17,44 +51,215 @@
 #define PIN_LAYOUT_IDENT "Non-Beta"
 #define __XRP_NONBETA
 
+/*
+// Motor and Servo Outputs
+#define XRP_MOTOR_L_EN  39
+#define XRP_MOTOR_L_PH  38
+#define XRP_MOTOR_R_EN  31
+#define XRP_MOTOR_R_PH  30
+#define XRP_MOTOR_3_EN  23
+#define XRP_MOTOR_3_PH  22
+#define XRP_MOTOR_4_EN  11
+#define XRP_MOTOR_4_PH  10
+#define XRP_SERVO_1     25
+#define XRP_SERVO_2     7
+#define XRP_SERVO_3     24
+#define XRP_SERVO_4     6
+
+// Encoders
+#define XRP_ENC_L_A     36
+#define XRP_ENC_L_B     37
+#define XRP_ENC_R_A     28
+#define XRP_ENC_R_B     29
+#define XRP_ENC_3_A     20
+#define XRP_ENC_3_B     21
+#define XRP_ENC_4_A     8
+#define XRP_ENC_4_B     9
+
+// Line Reflectors
+#define REFLECT_LEFT_PIN 44
+#define REFLECT_RIGHT_PIN 45
+
+// Range Finder
+#define ULTRASONIC_TRIG_PIN 0
+#define ULTRASONIC_ECHO_PIN 1
+
+// I2C IMU
+#define XRP_SCL 27
+#define XRP_SDA 26
+
+// User Button
+#define XRP_USER_BUTTON 2
+*/
+
+// Motors
+#define MOTOR_L_IN_1 34
+#define MOTOR_L_IN_2 35 
+#define MOTOR_R_IN_1 32
+#define MOTOR_R_IN_2 33
+#define MOTOR_3_IN_1 20
+#define MOTOR_3_IN_2 21
+#define MOTOR_4_IN_1 10
+#define MOTOR_4_IN_2 11
+
+// Encoders
+#define MOTOR_L_ENCODER_A     30
+#define MOTOR_L_ENCODER_B     31
+#define MOTOR_R_ENCODER_A     24
+#define MOTOR_R_ENCODER_B     25
+#define MOTOR_3_ENCODER_A     22
+#define MOTOR_3_ENCODER_B     23
+#define MOTOR_4_ENCODER_A     2
+#define MOTOR_4_ENCODER_B     3
+
+// SERVOS
+#define SERVO_1    6
+#define SERVO_2    9
+#define SERVO_3    7
+#define SERVO_4    8
+
+// Line Reflectors
+#define LINE_L 44
+#define LINE_R 45
+
+// Range Finder
+#define DISTANCE_TRIGGER 0
+#define DISTANCE_ECHO    1
+
+// I2C IMU
+#define I2C_SCL_1 39
+#define I2C_SDA_1 38
+
+// User Button
+#define BOARD_USER_BUTTON 36
+
+/*
+// Motor and Servo Outputs
+#define XRP_MOTOR_L_EN  35
+#define XRP_MOTOR_L_PH  34
+#define XRP_MOTOR_R_EN  33
+#define XRP_MOTOR_R_PH  32
+#define XRP_MOTOR_3_EN  21
+#define XRP_MOTOR_3_PH  20
+#define XRP_MOTOR_4_EN  11
+#define XRP_MOTOR_4_PH  10
+#define XRP_SERVO_1     6
+#define XRP_SERVO_2     9
+#define XRP_SERVO_3     7
+#define XRP_SERVO_4     8
+
+// Encoders
+#define XRP_ENC_L_A     30
+#define XRP_ENC_L_B     31
+#define XRP_ENC_R_A     24
+#define XRP_ENC_R_B     25
+#define XRP_ENC_3_A     22
+#define XRP_ENC_3_B     23
+#define XRP_ENC_4_A     2
+#define XRP_ENC_4_B     3
+
+// Line Reflectors
+#define REFLECT_LEFT_PIN 44
+#define REFLECT_RIGHT_PIN 45
+
+// Range Finder
+#define ULTRASONIC_TRIG_PIN 0
+#define ULTRASONIC_ECHO_PIN 1
+
+// I2C IMU
+#define XRP_SCL 39
+#define XRP_SDA 38
+
+// User Button
+#define XRP_USER_BUTTON 36
+*/
+
 #else
 // Beta
 #define PIN_LAYOUT_IDENT "Beta"
 
-/****************************************************************************************/
-/******** Keep below until can upgrade platformio to sparkfun_xrp_controller_beta *******/
-/****************************************************************************************/
-#define MOTOR_L_IN_1 (6u)
-#define MOTOR_L_IN_2 (7u)
-#define MOTOR_R_IN_1 (14u)
-#define MOTOR_R_IN_2 (15u)
-#define MOTOR_3_IN_1 (2u)
-#define MOTOR_3_IN_2 (3u)
-#define MOTOR_4_IN_1 (10u)
-#define MOTOR_4_IN_2 (11u)
-#define MOTOR_L_ENCODER_A (4u)
-#define MOTOR_L_ENCODER_B (5u)
-#define MOTOR_R_ENCODER_A (12u)
-#define MOTOR_R_ENCODER_B (13u)
-#define MOTOR_3_ENCODER_A (0u)
-#define MOTOR_3_ENCODER_B (1u)
-#define MOTOR_4_ENCODER_A (8u)
-#define MOTOR_4_ENCODER_B (9u)
-#define SERVO_1 (16u)
-#define SERVO_2 (17u)
-#define I2C_SDA_1 (18u)
-#define I2C_SCL_1 (19u)
-#define DISTANCE_TRIGGER (20u)
-#define DISTANCE_ECHO (21u)
-#define LINE_L (26u)
-#define LINE_R (27u)
-#define BOARD_VIN_MEASURE (28u)
-#define BOARD_USER_BUTTON (22u)
-#define BOARD_LED (PIN_LED)
-/*******************************************************************************/
-/*******************************************************************************/
+// XRP Pin Functions
 
+// Motors
+#define MOTOR_L_IN_1 6
+#define MOTOR_L_IN_2 7 
+#define MOTOR_R_IN_1 14
+#define MOTOR_R_IN_2 15
+#define MOTOR_3_IN_1 2
+#define MOTOR_3_IN_2 3
+#define MOTOR_4_IN_1 10
+#define MOTOR_4_IN_2 11
+
+// Encoders
+#define MOTOR_L_ENCODER_A     4
+#define MOTOR_L_ENCODER_B     5
+#define MOTOR_R_ENCODER_A     12
+#define MOTOR_R_ENCODER_B     13
+#define MOTOR_3_ENCODER_A     0
+#define MOTOR_3_ENCODER_B     1
+#define MOTOR_4_ENCODER_A     8
+#define MOTOR_4_ENCODER_B     9
+
+// SERVOS
+#define SERVO_1    16
+#define SERVO_2    17
 #define SERVO_3    __XRP_PIN_UNDEF
 #define SERVO_4    __XRP_PIN_UNDEF
+
+// Line Reflectors
+#define LINE_L 26
+#define LINE_R 27
+
+// Range Finder
+#define DISTANCE_TRIGGER 20
+#define DISTANCE_ECHO    21
+
+// I2C IMU
+#define I2C_SCL_1 19
+#define I2C_SDA_1 18
+
+// User Button
+#define BOARD_USER_BUTTON 22
+
+/*
+#define XRP_MOTOR_L_EN 7
+#define XRP_MOTOR_L_PH 6
+#define XRP_MOTOR_R_EN 15
+#define XRP_MOTOR_R_PH 14
+#define XRP_MOTOR_3_EN 3
+#define XRP_MOTOR_3_PH 2
+#define XRP_MOTOR_4_EN 11
+#define XRP_MOTOR_4_PH 10
+#define XRP_SERVO_1    16
+#define XRP_SERVO_2    17
+#define XRP_SERVO_3    __XRP_PIN_UNDEF
+#define XRP_SERVO_4    __XRP_PIN_UNDEF
+
+// Encoders
+#define XRP_ENC_L_A     4
+#define XRP_ENC_L_B     5
+#define XRP_ENC_R_A     12
+#define XRP_ENC_R_B     13
+#define XRP_ENC_3_A     0
+#define XRP_ENC_3_B     1
+#define XRP_ENC_4_A     8
+#define XRP_ENC_4_B     9
+
+
+// Line Reflectors
+#define REFLECT_LEFT_PIN 26
+#define REFLECT_RIGHT_PIN 27
+
+// Range Finder
+#define ULTRASONIC_TRIG_PIN 20
+#define ULTRASONIC_ECHO_PIN 21
+
+// I2C IMU
+#define XRP_SCL 19
+#define XRP_SDA 18
+
+// User Button
+#define XRP_USER_BUTTON 22
+*/
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <Servo.h>
 #include <vector>
 
+// Servo pulses
+#define XRP_SERVO_MIN_PULSE_US 500
+#define XRP_SERVO_MAX_PULSE_US 2500
+
 uint32_t _lastUpdateTime = 0;
 boolean direction = false;
 double speed = 0.8;
@@ -43,8 +47,6 @@ void setup() {
   delay(5000);
   Serial.begin(115200);
 
-  pinMode(BOARD_USER_BUTTON, INPUT_PULLUP);
-
   // Left
   pinMode(MOTOR_L_IN_1, OUTPUT);
   pinMode(MOTOR_L_IN_2, OUTPUT);
@@ -62,10 +64,44 @@ void setup() {
   pinMode(MOTOR_4_IN_2, OUTPUT);
 
   _initServos();
+
+  pinMode(BOARD_USER_BUTTON, INPUT_PULLUP);
+
   digitalWrite(LED_BUILTIN, HIGH);
 
   Serial.println("Initialization Complete\n");
 }
+
+
+#ifdef PICO_RP2350
+
+void _setMotorPwmValueInternal(int in2, int in1, double value) {
+  boolean is_forward = (value >= 0.0);
+  int speed = (abs(value) * 255);
+  
+  // Direction determines which pin should be the brake
+  if(is_forward) {
+    digitalWrite(in2,LOW);
+    analogWrite(in1, speed);
+  } else {
+    digitalWrite(in1,LOW);
+    analogWrite(in2, speed);    
+  }
+}
+
+#else
+
+void _setMotorPwmValueInternal(int en, int ph, double value) {
+  
+  PinStatus phValue = (value < 0.0) ? LOW : HIGH;
+  int enValue = (abs(value) * 255);
+
+  digitalWrite(ph, phValue);
+  analogWrite(en, enValue);
+}
+
+#endif
+
 
 void loop() {
 
@@ -77,18 +113,8 @@ void loop() {
 
   if( direction ) {
     digitalWrite(LED_BUILTIN, HIGH);
-
-#ifdef PICO_RP2350
-    digitalWrite(MOTOR_L_IN_2,LOW);
-    analogWrite(MOTOR_L_IN_1, speed);
-    digitalWrite(MOTOR_R_IN_2,LOW);
-    analogWrite(MOTOR_R_IN_1, speed);
-#else
-    digitalWrite(MOTOR_L_IN_2, LOW);
-    analogWrite(MOTOR_L_IN_1, speed);
-    digitalWrite(MOTOR_R_IN_2, LOW);
-    analogWrite(MOTOR_R_IN_1, speed);
-#endif
+    _setMotorPwmValueInternal(MOTOR_L_IN_2, MOTOR_L_IN_1, 0.8);
+    _setMotorPwmValueInternal(MOTOR_R_IN_2, MOTOR_R_IN_1, 0.8);
 
     for(int i = 0; i < NUM_OF_SERVOS; i++) {
       // Set different servo positions per servo port
@@ -98,18 +124,8 @@ void loop() {
     }
   } else {
     digitalWrite(LED_BUILTIN, LOW);
-
-#ifdef PICO_RP2350
-    digitalWrite(MOTOR_L_IN_1,LOW);
-    analogWrite(MOTOR_L_IN_2, speed);
-    digitalWrite(MOTOR_R_IN_1,LOW);
-    analogWrite(MOTOR_R_IN_2, speed);
-#else
-    digitalWrite(MOTOR_L_IN_2, HIGH);
-    analogWrite(MOTOR_L_IN_1, speed);
-    digitalWrite(MOTOR_R_IN_2, HIGH);
-    analogWrite(MOTOR_R_IN_1, speed);
-#endif
+    _setMotorPwmValueInternal(MOTOR_L_IN_2, MOTOR_L_IN_1, -0.8);
+    _setMotorPwmValueInternal(MOTOR_R_IN_2, MOTOR_R_IN_1, -0.8);
 
     // Reset Motors to zero and servo to 90 degs
     for(int i = 0;i < NUM_OF_SERVOS; i++) {
